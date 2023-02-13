14667ebb629dc9c5f3e483d4f1ad53223473277d
Cahya Wibawa <cahyawbwa.com>
2023-02-13 15:45:05 +0700

    add archivery title

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,9 @@ type Image = {
 export default function Gallery({ images }: { images: Image[] }) {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h1 className="text-center text-3xl font-bold uppercase tracking-normal pb-7 text-gray-700">
+        Archivery
+      </h1>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
